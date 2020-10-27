c82b998b876bbbac9a539d59522972f663561d6d
Janet Blohn <jblohn@my365.bellevue.edu>
2020-10-27 15:54:00 -0500

    Janet - Update Base Layout component, and Logo picture

--- src/app/shared/base-layout/base-layout.component.ts
+++ src/app/shared/base-layout/base-layout.component.ts
@@ -1,16 +1,17 @@
 /**
  * ======================================================================
  * Title: Bob's Computer Repair Shop Capstone
- * Author: Nicole Forke, Janet Blohn, Verlee Washington, and Joann Saeou
+ * Author: Nicole Forke, Janet Blohn, and Joann Saeou
  * Date: 10/25/2020
  * Description: Base Layout Component
+ * Added logOut method 10/27 Janet
  * ======================================================================
  */
 
  /**
   * Import statements
   */
- import { Component, OnInit } from '@angular/core';
+ import { Component, OnInit, ElementRef } from '@angular/core';
  import { Router } from '@angular/router';
  import { CookieService } from 'ngx-cookie-service';
  
@@ -19,24 +20,30 @@
    templateUrl: './base-layout.component.html',
    styleUrls: ['./base-layout.component.css']
  })
- 
+
  /**
   * Export component
   */
  export class BaseLayoutComponent implements OnInit {
- 
+
    year: number = Date.now();
-   constructor(private cookieService: CookieService, private router: Router) {
- 
+   constructor(private cookieService: CookieService, private router: Router, private elementRef: ElementRef) {
+     // Clear cookies if the user hits the "X" on their browser instead of logging out
+     window.onbeforeunload = function() {
+       console.log(this.sessionUser);
+       cookieService.deleteAll('sessionUser')
+     };
+
     }
- 
+
    ngOnInit(): void {
    }
- 
-   signOut() {
+
+   // Delete cookies on sign out and return to the home screen
+   logOut() {
      this.cookieService.deleteAll();
-     this.router.navigate(['/pages/signin']);
+     //this.router.navigate(['/pages/signin']);
+     this.router.navigate(['/pages/home']);
    }
- 
+
  }
- 
