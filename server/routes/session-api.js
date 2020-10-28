ee71731e01d15f5adc3d5154e274eb7deae99d34
nforke <n.forke@gmail.com>
2020-10-27 22:08:38 -0500

    Adding commit for session-api.js

--- server/routes/session-api.js
+++ server/routes/session-api.js
@@ -30,7 +30,6 @@ const router = express.Router();
  * ==============================================================================
  **/
 router.post('/signin', async(req, res) => {
-<<<<<<< HEAD
     try {
 
         User.findOne({ 'userName': req.body.userName }, function(err, user) {
@@ -81,62 +80,6 @@ router.post('/signin', async(req, res) => {
         const signinCatchErrorResponse = new ErrorResponse(500, 'Internal server error', e.message);
         res.status(500).send(signinCatchErrorResponse.toObject());
     }
-=======
-  try
-  {
-    User.findOne({'userName': req.body.userName}, function(err, user)
-    {
-      if (err)
-      {
-        console.log(err);
-        const signinMongodbErrorResponse = new ErrorResponse(500, 'Internal server error', err);
-        res.status(500).send(signinMongodbErrorResponse.toObject());
-      }
-
-      else
-      {
-        console.log(user);
-
-        // if the username is valid
-        if (user)
-        {
-          let passwordIsValid = bcrypt.compareSync(req.body.password, user.password);// compare the saved hashed password
-
-          // if the password is valid
-          if (passwordIsValid)
-          {
-            console.log(`Login successful`);
-            const signinResponse = new BaseResponse(200, 'Login successful', user);
-            res.json(signinResponse.toObject());
-          }
-
-          // if the password is not invalid
-          else
-          {
-            console.log( `Invalid password for username: ${user.userName}`);
-            const invalidPasswordResponse = new BaseResponse(401, 'Invalid username and/or password, please try again', null);
-            res.status(401).send(invalidPasswordResponse.toObject());
-          }
-        }
-
-        // if the username is invalid
-        else
-        {
-          console.log(`Username: ${req.body.userName} is invalid`);
-          const invalidUserNameResponse = new BaseResponse(401, 'Invalid username and/or password, please try again', null);
-          res.status(401).send(invalidUserNameResponse.toObject());
-        }
-      }
-    })
-  }
-
-  catch(e)
-  {
-    console.log(e);
-    const signinCatchErrorResponse = new ErrorResponse(500, 'Internal server error', e.message);
-    res.status(500).send(signinCatchErrorResponse.toObject());
-  }
->>>>>>> 204fa81bc976db11aee777c757ad0af5767e59d9
 });
 
 
