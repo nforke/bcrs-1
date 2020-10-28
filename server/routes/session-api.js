c2ac498bc08adf196ec4428ade7e3484996d458b
Joann Saeou <joannssaeou@gmail.com>
2020-10-27 19:51:07 -0700

    moved the logo-image to the center

--- server/routes/session-api.js
+++ server/routes/session-api.js
@@ -30,25 +30,17 @@ const router = express.Router();
  * ==============================================================================
  **/
 router.post('/signin', async(req, res) => {
-<<<<<<< HEAD
     try {
-
         User.findOne({ 'userName': req.body.userName }, function(err, user) {
             if (err) {
-
                 console.log(err);
                 const signinMongodbErrorResponse = new ErrorResponse(500, 'Internal server error', err);
                 res.status(500).send(signinMongodbErrorResponse.toObject());
-
             } else {
-
-
                 console.log(user);
 
                 // if the username is valid
                 if (user) {
-
-
                     let passwordIsValid = bcrypt.compareSync(req.body.password, user.password); // compare the saved hashed password
 
                     // if the password is valid
@@ -58,9 +50,8 @@ router.post('/signin', async(req, res) => {
                         res.json(signinResponse.toObject());
                     }
 
-                    // if the password is invalid
+                    // if the password is not invalid
                     else {
-
                         console.log(`Invalid password for username: ${user.userName}`);
                         const invalidPasswordResponse = new BaseResponse(401, 'Invalid username and/or password, please try again', null);
                         res.status(401).send(invalidPasswordResponse.toObject());
@@ -76,67 +67,10 @@ router.post('/signin', async(req, res) => {
             }
         })
     } catch (e) {
-
         console.log(e);
         const signinCatchErrorResponse = new ErrorResponse(500, 'Internal server error', e.message);
         res.status(500).send(signinCatchErrorResponse.toObject());
     }
-=======
-  try
-  {
-    User.findOne({'userName': req.body.userName}, function(err, user)
-    {
-      if (err)
-      {
-        console.log(err);
-        const signinMongodbErrorResponse = new ErrorResponse(500, 'Internal server error', err);
-        res.status(500).send(signinMongodbErrorResponse.toObject());
-      }
-
-      else
-      {
-        console.log(user);
-
-        // if the username is valid
-        if (user)
-        {
-          let passwordIsValid = bcrypt.compareSync(req.body.password, user.password);// compare the saved hashed password
-
-          // if the password is valid
-          if (passwordIsValid)
-          {
-            console.log(`Login successful`);
-            const signinResponse = new BaseResponse(200, 'Login successful', user);
-            res.json(signinResponse.toObject());
-          }
-
-          // if the password is not invalid
-          else
-          {
-            console.log( `Invalid password for username: ${user.userName}`);
-            const invalidPasswordResponse = new BaseResponse(401, 'Invalid username and/or password, please try again', null);
-            res.status(401).send(invalidPasswordResponse.toObject());
-          }
-        }
-
-        // if the username is invalid
-        else
-        {
-          console.log(`Username: ${req.body.userName} is invalid`);
-          const invalidUserNameResponse = new BaseResponse(401, 'Invalid username and/or password, please try again', null);
-          res.status(401).send(invalidUserNameResponse.toObject());
-        }
-      }
-    })
-  }
-
-  catch(e)
-  {
-    console.log(e);
-    const signinCatchErrorResponse = new ErrorResponse(500, 'Internal server error', e.message);
-    res.status(500).send(signinCatchErrorResponse.toObject());
-  }
->>>>>>> 204fa81bc976db11aee777c757ad0af5767e59d9
 });
 
 
