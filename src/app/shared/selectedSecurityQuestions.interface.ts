941ad9749aac08120fd3e33474939dba884f0409
Janet Blohn <jblohn@my365.bellevue.edu>
2020-11-01 15:08:55 -0600

    Janet - add SelectedSecurityQuestions Interface

---
+++ src/app/shared/selectedSecurityQuestions.interface.ts
@@ -0,0 +1,17 @@
+/**
+ * =====================================================================
+ * Title: Bobs Computer Repair Shop Capstone
+ * Author: Nicole Forke, Janet Blohn, Joann Saeou, and Verlee Washington
+ * Date: 10/24/2020
+ * Description: User interface
+ * ======================================================================
+ */
+
+/**
+ * Export Interface
+ */
+export interface SelectedSecurityQuestions {
+    questionText: string;
+    answerText: string;
+
+  }
