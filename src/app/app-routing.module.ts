2e9af32cbe8d09bdef79657796609bc0907012cd
Janet Blohn <jblohn@my365.bellevue.edu>
2020-11-07 10:59:45 -0600

    Janet add paths for roles, and commented out paths for invoices

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,7 +21,7 @@
   import { SecurityQuestionListComponent } from './pages/security-question-list/security-question-list.component';
   import { SigninComponent } from './pages/signin/signin.component';
   import { UserListComponent } from './pages/user-list/user-list.component';
-  import { AuthLayoutComponent } from './shared/auth-layout/auth-layout.component';
+  //import { AuthLayoutComponent } from './shared/auth-layout/auth-layout.component';
   import { UserDetailsComponent } from './pages/user-details/user-details.component';
   import { AboutComponent } from './pages/about/about.component';
   import { OrdersComponent } from './pages/orders/orders.component';
@@ -31,7 +31,17 @@
   import { VerifyUsernameFormComponent } from './pages/verify-username-form/verify-username-form.component';
   import { NotFoundComponent } from './pages/not-found/not-found.component';
   import { ServerErrorComponent } from './pages/server-error/server-error.component';
-  import { AuthGuard } from './shared/auth.guard';
+  import { PurchasesByServiceGraphComponent } from './pages/purchases-by-service-graph/purchases-by-service-graph.component'; // Added 11/07/20 Janet
+  import { RoleCreateComponent } from './pages/role-create/role-create.component';  //Added 11/07 Janet
+  import { RoleListComponent } from './pages/role-list/role-list.component';  //Added 11/07 Janet
+  import { RoleDetailsComponent } from './pages/role-details/role-details.component';  //Added 11/07 Janet
+  /* The Invoice Components have not yet been created. Adding them to app.routing commented out so they can be easily added when completed 11/7 Janet
+  import { InvoiceCreateComponent } from './pages/invoice-create/invoice-create.component';  //Added 11/07 Janet
+  import { InvoiceListComponent } from './pages/invoice-list/invoice-list.component';  //Added 11/07 Janet
+  import { InvoiceDetailsComponent } from './pages/invoice-details/invoice-details.component';  //Added 11/07 Janet */
+  import { AuthGuard } from './shared/auth.guard'; // Added 11/07/20 Janet
+  import { RoleGuard } from './shared/role.guard'; // Added 11/07/20 Janet
+
 
   /**
    * Configure routes
@@ -49,6 +59,11 @@
         {
           path: 'about',
           component: AboutComponent
+        },
+        {
+          path: 'purchases-by-service-graph',
+          component: PurchasesByServiceGraphComponent  /*,
+          canActivate: 'RoleGuard'   will be implemented after testing - Janet*/
         },
 
         // The remaining paths listed as children here will need to be moved to an admin path once one is created. Leaving as is for now.
@@ -93,10 +108,36 @@
           canActivate: [AuthGuard]
         },
         {
-          path: 'orders',
-          component: OrdersComponent,
-          canActivate: [AuthGuard]
-        }
+          path: 'roles',
+          component: RoleListComponent /*,
+          canActivate: [AuthGuard]   Turn on after testing 11/7 Janet */
+        },
+        {
+          path: 'roles/roleId',
+          component: RoleDetailsComponent /*,
+          canActivate: [AuthGuard]   Turn on after testing 11/7 Janet */
+        },
+        {
+          path: 'roles/create/new',
+          component: RoleCreateComponent /*,
+          canActivate: [AuthGuard]   Turn on after testing 11/7 Janet */
+        },
+        /*   The Invoice Paths will be un-commented out once the components are created 11/7 Janet
+        {
+          path: 'invoices',
+          component: InvoiceListComponent //,
+          //canActivate: [AuthGuard]   Turn on after testing 11/7 Janet
+        },
+        {
+          path: 'invoices/invoiceId',
+          component: InvoiceDetailsComponent //,
+          //canActivate: [AuthGuard]   Turn on after testing 11/7 Janet
+        },
+        {
+          path: 'invoices/create/new',
+          component: InvoiceCreateComponent //,
+          //canActivate: [AuthGuard]   Turn on after testing 11/7 Janet
+        } */
       ],
     },
     {
